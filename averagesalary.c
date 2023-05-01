968f2769c21ade41aff3f2439dcf8bb496de7874
Frank Collins <fwc3rd@gmail.com>
2023-04-30 23:03:32 -0400

    fix issue #1)  / 1.1 / [11:02pm]
    https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
    Have only done 5 easy algos or so. I believe this is new algo; if not new to me. 
    
    Issue #1 is all of the issues pertaining to average-salary algorithm.
    There will be multiple issues that could range quite variably. 
    
    Using a:
    int[ 1.1, 1.2, 1.3] Float system to organize and make easy reference of.
    emboldened comments in the issue while signal which [ 1.1     ||     1.7 ] the commit would be referencing.

--- averagesalary.c
+++ averagesalary.c
@@ -1,6 +1,14 @@
 // bookmarking that this repo would be separated by algo. It wouldn't be a main bank on one file. Didn't think of that posting the other page.
 
-int list () {
-  
-  return 0;
+// double average(int salary[], int salarySize){
+double average(int* salary, int salarySize){
+    for (int i = 0; i < sizeof(salary); i++) {
+        printf(" my salary:\t %d \n", salary[i]);
+
+        if (salary[i+1] == NULL || salary[i+1] == '\0') { break;}
+    }
+    // printf("salarySize:\t %d \n", salarySize);
+
+    return 777;
+    
 }
